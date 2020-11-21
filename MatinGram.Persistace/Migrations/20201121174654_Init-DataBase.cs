0b0c6a1a63fd3402c1030a610e2ce648cfcbb5b6
Matin Babaki <matinking82@gmail.com>
2020-11-21 21:25:04 +0330

    Add Migraion

---
+++ MatinGram.Persistace/Migrations/20201121174654_Init-DataBase.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MatinGram.Persistace.Migrations
+{
+    public partial class InitDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MobileNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    Bio = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    UserInRole = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chatrooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChatroomType = table.Column<int>(type: "int", nullable: false),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatorId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chatrooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chatrooms_Users_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserImages",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserImages_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AdminInChatrooms",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    ChatroomId = table.Column<int>(type: "int", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminInChatrooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AdminInChatrooms_Chatrooms_ChatroomId",
+                        column: x => x.ChatroomId,
+                        principalTable: "Chatrooms",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_AdminInChatrooms_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChatroomImages",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ChatroomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatroomImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChatroomImages_Chatrooms_ChatroomId",
+                        column: x => x.ChatroomId,
+                        principalTable: "Chatrooms",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    SendDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SenderId = table.Column<long>(type: "bigint", nullable: false),
+                    ChatroomID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Chatrooms_ChatroomID",
+                        column: x => x.ChatroomID,
+                        principalTable: "Chatrooms",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserInChatrooms",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    ChatroomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInChatrooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserInChatrooms_Chatrooms_ChatroomId",
+                        column: x => x.ChatroomId,
+                        principalTable: "Chatrooms",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_UserInChatrooms_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminInChatrooms_ChatroomId",
+                table: "AdminInChatrooms",
+                column: "ChatroomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminInChatrooms_UserId",
+                table: "AdminInChatrooms",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatroomImages_ChatroomId",
+                table: "ChatroomImages",
+                column: "ChatroomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chatrooms_CreatorId",
+                table: "Chatrooms",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ChatroomID",
+                table: "Messages",
+                column: "ChatroomID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages",
+                column: "SenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserImages_UserId",
+                table: "UserImages",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserInChatrooms_ChatroomId",
+                table: "UserInChatrooms",
+                column: "ChatroomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserInChatrooms_UserId",
+                table: "UserInChatrooms",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_MobileNumber",
+                table: "Users",
+                column: "MobileNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true,
+                filter: "[Username] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdminInChatrooms");
+
+            migrationBuilder.DropTable(
+                name: "ChatroomImages");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "UserImages");
+
+            migrationBuilder.DropTable(
+                name: "UserInChatrooms");
+
+            migrationBuilder.DropTable(
+                name: "Chatrooms");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

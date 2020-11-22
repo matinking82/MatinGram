dc27bc36a057a5892690ad84e309218507b0eeb2
Matin Babaki <matinking82@gmail.com>
2020-11-22 16:40:46 +0330

    Add Attributes

--- MatinGram.ViewModels/ViewModels/Users/UserSigninViewModel.cs
+++ MatinGram.ViewModels/ViewModels/Users/UserSigninViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,10 @@ namespace MatinGram.ViewModels.ViewModels.Users
 {
     public record UserSigninViewModel
     {
-
+        [Required]
         public string MobileNumber { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
     }

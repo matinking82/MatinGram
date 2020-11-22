34ad62fb3c8fab4996ce96a8adf0309b0c2b0b56
Matin Babaki <matinking82@gmail.com>
2020-11-22 11:54:45 +0330

    Add AuthenticationController

---
+++ EndPoint.Site/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EndPoint.Site.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult Signin()
+        {
+            return View();
+        }
+    }
+}

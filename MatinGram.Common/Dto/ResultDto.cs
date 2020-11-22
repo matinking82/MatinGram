6bd179429ff4f6b051d2d5a0b786772e0fbd0500
Matin Babaki <matinking82@gmail.com>
2020-11-22 14:11:39 +0330

    Add ResultDto

---
+++ MatinGram.Common/Dto/ResultDto.cs
@@ -0,0 +1,22 @@
+﻿using MatinGram.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatinGram.Common.Dto
+{
+    public record ResultDto
+    {
+        public ServiceStatus Status { get; set; }
+        public string Message { get; set; }
+    }
+
+    public record ResultDto<T>
+    {
+        public T Data { get; set; }
+        public ServiceStatus Status { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ MatinGram.Common/Enums/ServiceStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatinGram.Common.Enums
+{
+    public enum ServiceStatus
+    {
+        Success,
+        SystemError,
+        NotFound,
+        InputParametersError,
+        AccessDenied,
+        Error
+    }
+}

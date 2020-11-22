a732c69bf98b8f50279dab5480013d44911c7059
Matin Babaki <matinking82@gmail.com>
2020-11-22 14:50:10 +0330

    Add BaseEntity

---
+++ MatinGram.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatinGram.Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+        public DateTime InsertTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+    }
+}

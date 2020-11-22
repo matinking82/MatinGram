cbb654a80044c13cf5112bfd7cf979671577853a
Matin Babaki <matinking82@gmail.com>
2020-11-22 12:32:51 +0330

    Add Route Attribute

--- EndPoint.Site/Controllers/AuthenticationController.cs
+++ EndPoint.Site/Controllers/AuthenticationController.cs
@@ -9,12 +9,14 @@ namespace EndPoint.Site.Controllers
     public class AuthenticationController : Controller
     {
         [HttpGet]
+        [Route("/Signup")]
         public IActionResult Signup()
         {
             return View();
         }
         
         [HttpGet]
+        [Route("/Signin")]
         public IActionResult Signin()
         {
             return View();

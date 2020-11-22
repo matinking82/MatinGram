6ea7d429fabba90fcc78d6c29ef355d20a21b452
Matin Babaki <matinking82@gmail.com>
2020-11-22 14:10:50 +0330

    Add Password Hasher

---
+++ MatinGram.Common/Utilities/PasswordHasher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatinGram.Common.Utilities
+{
+    public static class PasswordHasher
+    {
+        public static string ToHashed(this string password)
+        {
+            using SHA256 sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var i in bytes)
+            {
+                builder.Append(i.ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}

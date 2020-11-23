87c4441820d9b50b7df69d7e8550ae7d951b8293
Matin Babaki <matinking82@gmail.com>
2020-11-23 11:51:19 +0330

    Add UserPanel To ViewComponent

---
+++ EndPoint.Site/ViewComponents/UserPanelInChatroom.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EndPoint.Site.ViewComponents
+{
+    public class UserPanelInChatroom : ViewComponent
+    {
+
+
+
+
+        public IViewComponentResult Invoke()
+        {
+            return View(viewName: "UserPanelInChatroom");
+        }
+    }
+}

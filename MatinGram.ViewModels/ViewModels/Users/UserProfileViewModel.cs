a0ccb7e79b58bb9fbf09f894052474a81ccde4fa
Matin Babaki <matinking82@gmail.com>
2020-11-23 13:19:01 +0330

    Add UserProfileViewModel

---
+++ MatinGram.ViewModels/ViewModels/Users/UserProfileViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatinGram.ViewModels.ViewModels.Users
+{
+    public record UserProfileViewModel
+    {
+        public long UserId { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Bio { get; set; }
+        public string ImageName { get; set; }
+        public string Username { get; set; }
+    }
+}

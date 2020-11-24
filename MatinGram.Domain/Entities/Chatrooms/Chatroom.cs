a4e1230db8059af1c323c5de792e282d950ae245
Matin Babaki <matinking82@gmail.com>
2020-11-24 11:22:41 +0330

    Add-Name-Chatroom

--- MatinGram.Domain/Entities/Chatrooms/Chatroom.cs
+++ MatinGram.Domain/Entities/Chatrooms/Chatroom.cs
@@ -15,6 +15,8 @@ namespace MatinGram.Domain.Entities.Chatrooms
     {
         public int Id { get; set; }
 
+        public string Name { get; set; }
+
         public ChatroomType ChatroomType { get; set; }
 
         public Guid Guid { get; set; }

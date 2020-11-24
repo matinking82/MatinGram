cd7b6bbfb9ff8d4cd87899a139c37009fede1810
Matin Babaki <matinking82@gmail.com>
2020-11-24 13:01:20 +0330

    Add ChatroomsController

---
+++ EndPoint.Site/Controllers/ChatroomsController.cs
@@ -0,0 +1,30 @@
+﻿using EndPoint.Site.Utilities;
+using MatinGram.Application.Interfaces.FacadPatterns;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EndPoint.Site.Controllers
+{
+    public class ChatroomsController : Controller
+    {
+        private readonly IChatroomsFacad _chatroomsFacad;
+        public ChatroomsController(IChatroomsFacad chatroomsFacad)
+        {
+            _chatroomsFacad = chatroomsFacad;
+        }
+        
+        public async Task<JsonResult> GetChatroomsList()
+        {
+
+            var UserId = User.GetUserId();
+
+
+            var result = await _chatroomsFacad.GetChatroomsByUserIdService.Execute(UserId);
+
+            return Json(result);
+        }
+    }
+}

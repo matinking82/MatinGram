623a962359adefb34d9770a195dd0f8259f96e72
Matin Babaki <matinking82@gmail.com>
2020-11-24 13:00:31 +0330

    Add ChatroomsListViewModel

---
+++ MatinGram.ViewModels/ViewModels/Chatrooms/ChatroomsListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatinGram.ViewModels.ViewModels.Chatrooms
+{
+    public record ChatroomsListViewModel
+    {
+        public string LastMessage { get; set; }
+        public string ChatroomName { get; set; }
+        public string ImageName { get; set; }
+        public string LastMessageTime { get; set; }
+        public Guid Guid { get; set; }
+    }
+}

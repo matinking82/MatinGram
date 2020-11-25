5cf296fa3ea467a3f63465f1301fb44cda62f077
Matin Babaki <matinking82@gmail.com>
2020-11-25 21:36:26 +0330

    Add Messages Controller

---
+++ EndPoint.Site/Controllers/MessagesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MatinGram.ViewModels.ViewModels.Messages;
+using MatinGram.Application.Interfaces.FacadPatterns;
+using MatinGram.Application.Services.Messages.Commands.AddNewMessage;
+using EndPoint.Site.Utilities;
+
+namespace EndPoint.Site.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private readonly IMessagesFacad _messagesFacad;
+        public MessagesController(IMessagesFacad messagesFacad)
+        {
+            _messagesFacad = messagesFacad;
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> AddMessage([Bind("Text,Guid")] AddNewMessageViewModel message)
+        {
+            RequestAddNewMessageDto request = new RequestAddNewMessageDto()
+            {
+                Guid = message.Guid,
+                Text = message.Text,
+                UserId = User.GetUserId()
+            };
+
+            var result = await _messagesFacad.AddNewMessageService.ExecuteAsync(request);
+
+            return Json(result);
+        }
+    }
+}

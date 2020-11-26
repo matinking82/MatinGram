47af89af00bbec1825f5879377c4ff9d927d534d
Matin Babaki <matinking82@gmail.com>
2020-11-26 18:23:58 +0330

    nothing

--- MatinGram.Application/Services/Chatrooms/Queries/GetChatroomDetailByUsername/IGetChatroomDetailByUsernameService.cs
+++ MatinGram.Application/Services/Chatrooms/Queries/GetChatroomDetailByUsername/IGetChatroomDetailByUsernameService.cs
@@ -16,6 +16,117 @@ namespace MatinGram.Application.Services.Chatrooms.Queries.GetChatroomDetailByUs
         Task<ResultDto<ChatroomDetailByUsernameDto>> Execute(long MyId, string TargetUsername);
     }
 
+
+    public class NewGetChatroomDetailByUsernameService : IGetChatroomDetailByUsernameService
+    {
+        private readonly IDataBaseContext _context;
+        public NewGetChatroomDetailByUsernameService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+        public async Task<ResultDto<ChatroomDetailByUsernameDto>> Execute(long MyId, string TargetUsername)
+        {
+
+            return await Task.Run(async () =>
+            {
+                try
+                {
+                    var targetUser = await _context.Users
+                    .FirstOrDefaultAsync(u => u.Username == TargetUsername);
+
+                    if (targetUser == null)
+                    {
+                        return new ResultDto<ChatroomDetailByUsernameDto>()
+                        {
+                            Status = Common.Enums.ServiceStatus.NotFound
+                        };
+                    }
+
+                    var PVChat = await _context.Chatrooms
+                    .Include(c => c.UserInChatrooms)
+                    .Where(c => c.ChatroomType == ChatroomType.PV)
+                    .FirstOrDefaultAsync(c => c.UserInChatrooms.Any(u => u.UserId == targetUser.Id) && c.UserInChatrooms.Any(u => u.UserId == MyId));
+                    
+                    
+                    #region --Take Data--
+                    var Data = new ChatroomDetailByUsernameDto();
+
+
+                    Data.ChatroomName = targetUser.Name;
+
+                    #region --find image--
+
+
+                    var userImage = await _context.UserImages
+                    .Where(u => u.UserId == targetUser.Id)
+                    .OrderBy(u => u.InsertTime)
+                    .LastOrDefaultAsync();
+
+                    Data.ImageName = userImage != null ? userImage.ImageName :
+                    "Images/UserImages/Default.png";
+
+
+                    #endregion
+
+                    #region --Take Messages--
+                    if (PVChat != null)
+                    {
+
+                        var messages = _context.Messages
+                        .Where(m => m.ChatroomID == PVChat.Id);
+
+                        Data.Messages = messages.Select(m => new MessageDto()
+                        {
+                            Date = m.SendDate,
+                            IsMe = m.SenderId == MyId,
+                            MessageId = m.Id,
+                            Text = m.Text
+                        });
+
+                        Data.ChatroomGuid = PVChat.Guid;
+                    }
+
+                    return new ResultDto<ChatroomDetailByUsernameDto>()
+                    {
+                        Status = ServiceStatus.Success,
+                        Data = Data
+                    };
+                    #endregion
+
+                    #endregion
+                }
+                catch (Exception)
+                {
+                    return new ResultDto<ChatroomDetailByUsernameDto>()
+                    {
+                        Status = Common.Enums.ServiceStatus.SystemError,
+                    };
+                }
+            });
+
+        }
+    }
+
+
+
+
+    public class ChatroomDetailByUsernameDto
+    {
+        public string ChatroomName { get; set; }
+        public string ImageName { get; set; }
+        public Guid ChatroomGuid { get; set; }
+        public IEnumerable<MessageDto> Messages { get; set; }
+    }
+
+    public class MessageDto
+    {
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsMe { get; set; }
+        public long MessageId { get; set; }
+    }
+
+
     public class GetChatroomDetailByUsernameService : IGetChatroomDetailByUsernameService
     {
         private readonly IDataBaseContext _context;
@@ -101,121 +212,5 @@ namespace MatinGram.Application.Services.Chatrooms.Queries.GetChatroomDetailByUs
                 return userInChatroom.Chatroom;
             }
         }
-    }
-
-
-
-
-
-
-
-
-
-    public class NewGetChatroomDetailByUsernameService : IGetChatroomDetailByUsernameService
-    {
-        private readonly IDataBaseContext _context;
-        public NewGetChatroomDetailByUsernameService(IDataBaseContext context)
-        {
-            _context = context;
-        }
-        public async Task<ResultDto<ChatroomDetailByUsernameDto>> Execute(long MyId, string TargetUsername)
-        {
-
-            return await Task.Run(async () =>
-            {
-                try
-                {
-                    var targetUser = await _context.Users
-                    .FirstOrDefaultAsync(u => u.Username == TargetUsername);
-
-
-
-                    if (targetUser == null)
-                    {
-                        return new ResultDto<ChatroomDetailByUsernameDto>()
-                        {
-                            Status = Common.Enums.ServiceStatus.NotFound
-                        };
-                    }
-
-
-
-                    var PVChat = await _context.Chatrooms
-                    .Include(c => c.UserInChatrooms)
-                    .Where(c => c.ChatroomType == ChatroomType.PV)
-                    .FirstOrDefaultAsync(c => c.UserInChatrooms.Any(u => u.UserId == targetUser.Id) && c.UserInChatrooms.Any(u => u.UserId == MyId));
-
-                    var Data = new ChatroomDetailByUsernameDto();
-
-
-                    Data.ChatroomName = targetUser.Name;
-                    #region --find image--
-
-
-                    var userImage = await _context.UserImages
-                    .Where(u => u.UserId == targetUser.Id)
-                    .OrderBy(u => u.InsertTime)
-                    .LastOrDefaultAsync();
-
-                    Data.ImageName = userImage != null ? userImage.ImageName :
-                    "Images/UserImages/Default.png";
-
-
-                    #endregion
-
-
-                    if (PVChat != null)
-                    {
-
-                        var messages = _context.Messages
-                        .Where(m => m.ChatroomID == PVChat.Id);
-
-                        Data.Messages = messages.Select(m => new MessageDto()
-                        {
-                            Date = m.SendDate,
-                            IsMe = m.SenderId == MyId,
-                            MessageId = m.Id,
-                            Text = m.Text
-                        });
-
-                        Data.ChatroomGuid = PVChat.Guid;
-                    }
-
-                    return new ResultDto<ChatroomDetailByUsernameDto>()
-                    {
-                        Status = ServiceStatus.Success,
-                        Data = Data
-                    };
-
-                }
-                catch (Exception)
-                {
-                    return new ResultDto<ChatroomDetailByUsernameDto>()
-                    {
-                        Status = Common.Enums.ServiceStatus.SystemError,
-                    };
-                }
-            });
-
-        }
-    }
-
-
-
-
-    public class ChatroomDetailByUsernameDto
-    {
-        public string ChatroomName { get; set; }
-        public string ImageName { get; set; }
-        public Guid ChatroomGuid { get; set; }
-        public IEnumerable<MessageDto> Messages { get; set; }
-    }
-
-    public class MessageDto
-    {
-        public string Text { get; set; }
-        public DateTime Date { get; set; }
-        public bool IsMe { get; set; }
-        public long MessageId { get; set; }
     }
 }

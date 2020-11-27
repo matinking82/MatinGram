996f077a56dc2a3095ef698537be7a9147548f22
Matin Babaki <matinking82@gmail.com>
2020-11-27 16:30:44 +0330

    Add CreateNewGroupViewModel

---
+++ MatinGram.ViewModels/ViewModels/Chatrooms/CreateNewGroupViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatinGram.ViewModels.ViewModels.Chatrooms
+{
+    public record CreateNewGroupViewModel
+    {
+        public string GroupName { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
